f2c88f185850f47fa3076a822c25214c1f45c156
tbutterwith <tbutterwith@gmail.com>
2015-03-20 16:05:22 +0000

    updated lambda.ml to parse Lambda.expr to list

--- Arithmetic Compiler/lambda.ml
+++ Arithmetic Compiler/lambda.ml
@@ -13,13 +13,25 @@ let mult	= Lambda ( "mnfx", String "m(nf)x" )
 let rec lambda_to_string expr = 
 	match expr with 
 	| String e 				-> e
-	| Lambda (id, expr1) 	-> "\\" ^ id ^ "." ^ (lambda_to_string expr1)
-	| App (expr1, expr2)	-> "(" ^ lambda_to_string expr1 ^ ")" ^	"(" ^ lambda_to_string expr2 ^ ")"	
+	| Lambda (id, expr1) 	-> "(\\" ^ id ^ "." ^ (lambda_to_string expr1) ^ ")"
+	| App (expr1, expr2)	-> lambda_to_string expr1  ^ lambda_to_string expr2	
+
+let rec extract_lambdas expr expr_list = 
+	match expr with
+	| String e 				-> expr_list @ []
+	| Lambda (id, expr1) 	-> expr_list @ (Lambda (id, expr1) :: [])
+	| App (expr1, expr2)	-> expr_list @ extract_lambdas expr1 [] @ extract_lambdas expr2 []
+
+let rec create_lambda_list expr_list new_list = 
+	match expr_list with
+	| [] 	-> new_list
+	| x::y 	-> let extractedList = new_list @ (extract_lambdas x []) in
+				create_lambda_list y extractedList
+
 
 let rec expand_church expr = 
 	match expr with
-	| 0 -> ""
-	| 1 -> "s(z)"
+	| 0 -> "z"
 	| _ -> "s(" ^ (expand_church (expr - 1)) ^ ")"
 
 let int_to_church expr = 
@@ -28,3 +40,6 @@ let int_to_church expr =
 	| 1 -> Lambda ( "sz", String "s(z)" )
 	| _ -> Lambda ( "sz", String ( expand_church expr )) 
 
+let beta_simplification expr = 
+	let expr_list = create_lambda_list expr [] in 
+		Lambda ("test", String "testing")
--- Arithmetic Compiler/math.ml
+++ Arithmetic Compiler/math.ml
@@ -3,13 +3,13 @@ open Lexer
 open Printf
 open Lambda
 
-let rec parse_channel lexbuf = 
+let rec parse_channel lexbuf  expr_list= 
 	let output = Parser.main Lexer.read lexbuf in
 	match output with
 	| Some c -> 
-		print_endline (lambda_to_string c);
-		parse_channel lexbuf
-	| None -> ()
+		parse_channel lexbuf (c::expr_list)
+	| None -> let simplified = beta_simplification expr_list in 
+			print_endline( lambda_to_string simplified)
 	
 
 let  () = 
@@ -17,5 +17,5 @@ let  () =
 	let inx = In_channel.create filename in
 	let lexbuf = Lexing.from_channel inx in
 	lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename }; 
-	parse_channel lexbuf;
+	parse_channel lexbuf [];
 	In_channel.close inx
